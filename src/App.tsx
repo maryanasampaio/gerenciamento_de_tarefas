54a21b9455fe8d3e9672641ac0f9a73492e13dc6
Maryana Sampaio <maryanacem.star2@gmail.com>
2025-09-18 11:06:49 -0300

    Add => Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red">
+      <h1 className="text-3xl font-bold text-blue-500">
+        teste
+      </h1>
+    </div>
+  );
+}
+
+
+
